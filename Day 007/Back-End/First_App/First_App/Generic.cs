b330d42e54b6e045e7819e442257baa2eddabf62
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-03-28 20:19:25 +0200

    Added Day 7

---
+++ Day 007/Back-End/First_App/First_App/Generic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App
+{
+    internal class Generic<TValue1, TValue2>
+    {
+        private string[] nomi1 = new string[5];
+        private string[] nomi2 = new string[5];
+        private TValue1[] valori1 = new TValue1[5];
+        private TValue2[] valori2 = new TValue2[5];
+        public void Add(string nome1, string nome2, TValue1 valore1, TValue2 valore2)
+        {
+            for (int i = 0; i < nomi1.Length; i++)
+            {
+                if (nomi1[i] == null)
+                {
+                    nomi1[i] = nome1;
+                    nomi2[i] = nome2;
+                    valori1[i] = valore1;
+                    valori2[i] = valore2;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Day 007/Back-End/First_App/First_App/Program.cs
@@ -0,0 +1,21 @@
+﻿using First_App;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Generic<string, DateTime> identità = new();
+        identità.Add("Nome e Cognome", "Data di Nascita", "Daniele Ghilardini", new DateTime(1999, 9, 7));
+
+        ClassToSerialize c = new ClassToSerialize();
+        File f = new File("temp.dat");
+        Stream s = f.Open(FileMode.Create);
+        BinaryFormatter b = new BinaryFormatter();
+        b.Serialize(s, c);
+        s.Close();
+    }
+}
