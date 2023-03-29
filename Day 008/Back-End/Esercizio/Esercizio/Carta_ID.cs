338dc6df67e46329df609c854d71b33ab0eb5526
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-03-29 20:39:26 +0200

    Added Front-End to Day 8

---
+++ Day 008/Back-End/Esercizio/Esercizio/Carta_ID.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio
+{
+    internal class Carta_ID
+    {
+        public string Cognome {
+            get
+            {
+                return Cognome;
+            }
+            set
+            {
+                if(hasNumber(Cognome))
+                {
+                    Cognome = value;
+                }
+                else
+                {
+                    throw new Exception("Ci sono numeri, il cognome è invalido");
+                }
+            }
+        }
+        public string Nome 
+        {
+            get
+            {
+                return Nome;
+            }
+            set
+            {
+                if (hasNumber(Nome))
+                {
+                    Nome = value;
+                }
+                else
+                {
+                    throw new Exception("Ci sono numeri, il nome è invalido");
+                }
+            }
+        }
+        public string CittaResidenza 
+        {
+            get
+            {
+                return CittaResidenza;
+            }
+            set
+            {
+                if (hasNumber(CittaResidenza))
+                {
+                    CittaResidenza = value;
+                }
+                else
+                {
+                    throw new Exception("Ci sono numeri, la città di residenza è invalida");
+                }
+            }
+        }
+        public DateTime DataNascita
+        {
+            get
+            {
+                return DataNascita;
+            }
+            set
+            {
+                DateTime limite = new DateTime(1900, 1, 1);
+                int compare = DateTime.Compare(DataNascita, limite);
+                if (compare >= 0)
+                {
+                    DataNascita = value;
+                }
+                else
+                {
+                    throw new Exception("La data di nascita è invalida");
+                }
+            }
+        }
+        public char Sesso { get; set; }
+        public int Statura { get; set; }
+
+        private bool hasNumber(string stringa)
+        {
+            bool number = false;
+            for(int i = 0; i < stringa.Length; i++) {
+                if (Char.IsDigit(stringa[i])) {
+                    number = true;
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ Day 008/Back-End/Esercizio/Esercizio/Program.cs
@@ -0,0 +1,11 @@
+﻿using Esercizio;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Carta_ID id = new Carta_ID();
+        id.Nome = "Daniele";
+        (id.Nome).All(char.IsDigit)
+    }
+}
