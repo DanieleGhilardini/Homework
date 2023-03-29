d70ba9226abe43f29d04d8dce84d3ff045eb81ae
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-03-29 11:44:22 +0200

    Day 8

---
+++ Day 007/Back-End/First_App/First_App/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App
+{
+    internal class Class1
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime Nascita { get; set; }
+    }
+}
--- Day 007/Back-End/First_App/First_App/Program.cs
+++ Day 007/Back-End/First_App/First_App/Program.cs
@@ -1,21 +1,30 @@
 ﻿using First_App;
-using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Generic<string, DateTime> identità = new();
-        identità.Add("Nome e Cognome", "Data di Nascita", "Daniele Ghilardini", new DateTime(1999, 9, 7));
+       // Generic<string, DateTime> identità = new();
+       // identità.Add("Nome e Cognome", "Data di Nascita", "Daniele Ghilardini", new DateTime(1999, 9, 7));
 
-        ClassToSerialize c = new ClassToSerialize();
-        File f = new File("temp.dat");
-        Stream s = f.Open(FileMode.Create);
-        BinaryFormatter b = new BinaryFormatter();
-        b.Serialize(s, c);
-        s.Close();
+        Class1 class1 = new Class1();
+        class1.Nome = "Daniele";
+        class1.Cognome = "Ghilardini";
+        class1.Nascita = new DateTime (1999, 9, 7);
+
+        string jsonSerializer = JsonConvert.SerializeObject(class1);
+
+        using (StreamWriter sw = new StreamWriter("DriveDirs.txt"))
+        {
+            sw.WriteLine(jsonSerializer);
+        }
+
+        string readJson;
+        using(StringReader sr = new StringReader("DriveDirs.txt"))
+        {
+            readJson = sr.ReadToEnd();
+        }
+        Class1 deserializedJson = JsonConvert.DeserializeObject<Class1>(readJson);
     }
 }
