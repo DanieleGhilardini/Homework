0f046a66ca45f88acefa57ef6d9ae1143ebf34ae
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-03-30 10:01:22 +0200

    Finished

--- Day 008/Back-End/Esercizio/Esercizio/Program.cs
+++ Day 008/Back-End/Esercizio/Esercizio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Esercizio;
+using Newtonsoft.Json;
 
 internal class Program
 {
@@ -6,6 +7,25 @@ internal class Program
     {
         Carta_ID id = new Carta_ID();
         id.Nome = "Daniele";
+        id.Cognome = "Ghilardini";
+        id.CittaResidenza = "Albino";
+        id.DataNascita = new DateTime(1999, 9, 7);
+        id.Sesso = 'M';
+        id.Statura = 190;
+
+        string jsonSerializer = JsonConvert.SerializeObject(id);
+
+        using (StreamWriter sw = new StreamWriter("DriveDirs.txt"))
+        {
+            sw.WriteLine(jsonSerializer);
+        }
+
+        string readJson;
+        using (StringReader sr = new StringReader("DriveDirs.txt"))
+        {
+            readJson = sr.ReadToEnd();
+        }
+        Carta_ID deserializedJson = JsonConvert.DeserializeObject<Carta_ID>(readJson);
         Console.ReadKey();
     }
 }
